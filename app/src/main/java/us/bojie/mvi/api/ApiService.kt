631a52312b7e77b37c7134a07c5cf41b3ed95019
Bojie Jiang <jiangbojie@gmail.com>
2019-10-17 21:11:50 -0700

    Models and API service

---
+++ app/src/main/java/us/bojie/mvi/api/ApiService.kt
@@ -0,0 +1,18 @@
+package us.bojie.mvi.api
+
+import androidx.lifecycle.LiveData
+import retrofit2.http.GET
+import retrofit2.http.Path
+import us.bojie.mvi.model.BlogPost
+import us.bojie.mvi.model.User
+import us.bojie.mvi.util.GenericApiResponse
+
+interface ApiService {
+    @GET("placeholder/blogs")
+    fun getBlogPost(): LiveData<GenericApiResponse<List<BlogPost>>>
+
+    @GET("placeholder/user/{userId}")
+    fun getUser(
+        @Path("userId") userId: String
+    ): LiveData<GenericApiResponse<User>>
+}
