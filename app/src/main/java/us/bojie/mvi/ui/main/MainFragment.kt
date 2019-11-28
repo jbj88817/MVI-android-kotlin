d93905255b80f5bd9a6bba1288b9cff9e2ad1c4b
Bojie Jiang <jiangbojie@gmail.com>
2019-11-27 20:32:24 -0800

    RecyclerView template and BlogListAdapter

--- app/src/main/java/us/bojie/mvi/ui/main/MainFragment.kt
+++ app/src/main/java/us/bojie/mvi/ui/main/MainFragment.kt
@@ -6,16 +6,28 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_main.*
 import us.bojie.mvi.R
+import us.bojie.mvi.model.BlogPost
 import us.bojie.mvi.ui.DataStateListener
 import us.bojie.mvi.ui.main.state.MainStateEvent.GetBlogPostsEvent
 import us.bojie.mvi.ui.main.state.MainStateEvent.GetUserEvent
+import us.bojie.mvi.util.TopSpacingItemDecoration
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(),
+    BlogListAdapter.Interaction {
+
+    override fun onItemSelected(position: Int, item: BlogPost) {
+        println("debug: clicked $position")
+        println("debug: clicked $item")
+    }
 
     lateinit var viewModel: MainViewModel
 
     lateinit var dataStateListener: DataStateListener
+
+    lateinit var blogListAdapter: BlogListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +46,17 @@ class MainFragment : Fragment() {
         } ?: throw Exception("Invalid activity")
 
         subscribeObservers()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(activity)
+            val topSpacingItemDecoration = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingItemDecoration)
+            blogListAdapter = BlogListAdapter(this@MainFragment)
+            adapter = blogListAdapter
+        }
     }
 
     private fun subscribeObservers() {
@@ -63,6 +86,7 @@ class MainFragment : Fragment() {
         viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
             viewState.blogPosts?.let {
                 println("DEBUG: setting blog posts to RecyclerView")
+                blogListAdapter.submitList(it)
             }
 
             viewState.user?.let {
